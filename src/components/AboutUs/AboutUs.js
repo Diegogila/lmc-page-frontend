23f85332b041fbd9c9d6f64b08bf202533718078
diegogil <dgg26496@hotmail.com>
2024-11-12 17:08:54 -0600

    Update

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -7,16 +7,18 @@ import right_photo from '../../assets/images/foto-derecha.png'
 function AboutUs(){
     return (
       <section className="about-us">
-        <h1>¿Quienes somos?</h1>
-        <p>
-          Somos una agencia de marketing con un espíritu humano y amable,
-          comprometida con el crecimiento y el éxito de nuestros clientes.
-          Nuestra misión es crear estrategias innovadoras y efectivas que
-          conecten con tu audiencia y generen resultados impactantes.
-        </p>
-        <CallToAction text="Conocenos mas"/>
-        <img className="image-left" src={left_photo} alt="Mariel y Sam"/>
-        <img className="image-right" src={right_photo} alt="Mariel y Sam"/>
+        <div className="about-us__content">
+          <h1>¿Quienes somos?</h1>
+          <p>
+            Somos una agencia de marketing con un espíritu humano y amable,
+            comprometida con el crecimiento y el éxito de nuestros clientes.
+            Nuestra misión es crear estrategias innovadoras y efectivas que
+            conecten con tu audiencia y generen resultados impactantes.
+          </p>
+          <CallToAction text="Conocenos mas"/>
+          <img className="image-left" src={left_photo} alt="Mariel y Sam"/>
+          <img className="image-right" src={right_photo} alt="Mariel y Sam"/>
+        </div>
       </section>
     );
 }
